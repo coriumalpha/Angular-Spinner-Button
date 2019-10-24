174ec7608ecabda73708bd1ff62060e8b46f1c92
Corium <toroidalmirror@gmail.com>
2019-10-24 14:00:29 +0200

    Simplify button instantiation

--- loading-button/loading-button.component.ts
+++ loading-button/loading-button.component.ts
@@ -1,93 +1,58 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
-import { Subject } from 'rxjs';
+import { Component, OnInit, Input } from '@angular/core';
+import { LoadingButtonContent } from './loading-button-content';
 
 @Component({
-  selector: 'app-loading-button',
-  templateUrl: './loading-button.component.html',
-  styleUrls: ['./loading-button.component.scss']
+	selector: 'loading-button',
+	templateUrl: './loading-button.component.html',
+	styleUrls: ['./loading-button.component.scss']
 })
 export class LoadingButtonComponent implements OnInit {
 
-  @Input() public buttonContent: any;
-  @Input() public loadingSubject: Subject<any>;
+	@Input() public buttonContent: LoadingButtonContent;
 
-  @Output() clickEvent: EventEmitter<any> = new EventEmitter();
+	constructor() { }
 
-  constructor() { }
+	public hasErrors: boolean = false;
+	public hasSucceeded: boolean = false;
 
-    public isLoading: boolean = false;
-    public hasErrors: boolean = false;
-    public hasSucceeded: boolean = false;
+	public errorClass: string = 'btn-danger';
+	public succeedClass: string = 'btn-primary';
+	public textClass: string = 'text-white';
 
-    public errorClass: string = 'btn-danger';
-    public succeedClass: string = 'btn-primary';
-    public textClass: string = 'text-white';
+	public btnIcon: string = "fa-floppy-o";
+	public btnText: string = "GUARDAR";
 
-    public btnIcon: string = "fa-floppy-o";
-    public btnText: string = "GUARDAR";
-    public isSmall: boolean = false;
+	ngOnInit() {
+		this.configureButtonContent();
 
-  ngOnInit() {
-    this.configureButtonContent();
+		this.buttonContent.LoadingSubject.subscribe(data => {
+			if (data === false) {
+				this.hasSucceeded = true;
+				setTimeout(x => {
+					this.hasSucceeded = false;
+				}, 3000);
+			} else if (data === "error") {
+				this.hasErrors = true;
+				setTimeout(x => {
+					this.hasErrors = false;
+				}, 4000);
+			}
+		});
+	}
 
-    this.loadingSubject.subscribe(data => {
-      if (data === true) {
-        this.isLoading = true;
-      }
-      if (data === false) {
-        this.hasSucceeded = true;
-        setTimeout(x => {
-          this.hasSucceeded = false;
-          this.isLoading = false;
-        }, 3000);
-      }
-      if (data === "error") {
-        this.hasErrors = true;
-        setTimeout(x => {
-          this.hasErrors = false;
-          this.isLoading = false;
-        }, 4000);
-      }
-    });
-  }
+	ngOnChange() {
+		this.configureButtonContent();
+	}
 
-  ngOnChange() {
-    this.configureButtonContent();
-  }
+	private configureButtonContent() {
+		if (this.buttonContent != null &&
+			this.buttonContent != undefined) {
 
-  onClick($event) {
-    this.clickEvent.next($event);
-  }
-
-  configureButtonContent() {
-    if (this.buttonContent != null &&
-      this.buttonContent != undefined) {
-
-      if (this.buttonContent.hasOwnProperty('icon') &&
-      this.buttonContent.icon != null &&
-      this.buttonContent.icon != undefined) {
-        this.btnIcon = this.buttonContent.icon;
-      }
-
-      if (this.buttonContent.hasOwnProperty('text') &&
-      this.buttonContent.text != null &&
-      this.buttonContent.text != undefined) {
-        this.btnText = this.buttonContent.text;
-      }
-
-      if (this.buttonContent.hasOwnProperty('isSmall') &&
-      this.buttonContent.isSmall != null &&
-      this.buttonContent.isSmall != undefined &&
-      this.buttonContent.isSmall) {
-        this.isSmall = true;
-      }
-
-      if (this.buttonContent.hasOwnProperty('isLight') &&
-      this.buttonContent.isLight) {
-        this.succeedClass = 'btn-secondary';
-        this.textClass = 'text-primary';
-      }
-    }
-  }
+			if (this.buttonContent.IsLight) {
+				this.succeedClass = 'btn-secondary';
+				this.textClass = 'text-primary';
+			}
+		}
+	}
 
 }
